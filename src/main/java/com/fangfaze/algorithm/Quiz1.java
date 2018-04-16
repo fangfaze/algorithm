488cc35593c5ad1faada510f3ebaec4f18f12c38
chenze <ace50@163.com>
2018-04-17 00:47:41 +0800

    Initial commit

---
+++ src/main/java/com/fangfaze/algorithm/Quiz1.java
@@ -0,0 +1,26 @@
+package com.fangfaze.algorithm;
+
+import java.util.List;
+
+public class Quiz1 {
+    /**
+     * 给定数组,求其中连续的部分,这部分之和最大
+     */
+
+    public Quiz1(int[] list) {
+        this.list = list;
+    }
+
+    public final int[] list;
+
+    public int max() {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int i : list) {
+            sum = Math.max(i, i + sum);
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
+¬
---
+++ src/test/java/com/fangfaze/algorithm/Quiz1Test.java
@@ -0,0 +1,22 @@
+package com.fangfaze.algorithm;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+/**
+ * Quiz1 Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>04/17/2018</pre>
+ */
+public class Quiz1Test {
+
+    @Test
+    public void testMax() throws Exception {
+        int[] a = {-1, 2};
+        System.out.println(new Quiz1(a).max());
+    }
+
+} 
