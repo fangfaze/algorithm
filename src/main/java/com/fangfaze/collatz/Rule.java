469b4761bb22b20ba1583149d5be1090386b605f
chenze <ace50@163.com>
2018-05-13 15:57:06 +0800

    陷入停滞.分段法并未发现明显的规律,尤其对11011

--- src/main/java/com/fangfaze/collatz/Rule.java
+++ src/main/java/com/fangfaze/collatz/Rule.java
@@ -8,11 +8,48 @@ public class Rule {
 
     public static List<Step> flow(BigInteger bigInteger) {
         List<Step> list = new LinkedList<>();
-        Step step = new Step(bigInteger);
-        while (!step.isOne()) {
+        for (Step step = new Step(bigInteger); !step.isOne(); step = new Step(step.getNext())) {
             list.add(step);
-            step = new Step(step.getNext());
         }
         return list;
     }
+
+
+    public static List<String> anaglize(BigInteger bigInteger) {
+        List<String> list = new LinkedList<>();
+        for (Step step = new Step(bigInteger); !step.isOne(); step = new Step(step.getNext())) {
+            if (step.isOdd()) {
+                String string = step.getValue().toString(2);
+                list.add(string + "=>" + getMax1(string));
+            }
+        }
+        return list;
+    }
+
+    public static int what(BigInteger bigInteger) {
+        int max = 0;
+        for (Step step = new Step(bigInteger); !step.isOne(); step = new Step(step.getNext())) {
+            int count = getMax1(step.getValue().toString(2));
+            max = max > count ? max : count;
+        }
+        return max;
+    }
+
+
+    public static int getMax1(String string) {
+        char[] chars = string.toCharArray();
+        int max = 0;
+        int count = 0;
+        for (int i = chars.length - 1; i >= 0; --i) {
+            if (chars[i] == '1') {
+                ++count;
+            } else {
+                max = max > count ? max : count;
+                count = 0;
+            }
+        }
+        max = max >= count ? max : count;
+        return max;
+    }
+
 }
--- src/test/java/com/fangfaze/collatz/RuleTest.java
+++ src/test/java/com/fangfaze/collatz/RuleTest.java
@@ -5,7 +5,9 @@ import org.junit.Before;
 import org.junit.After;
 
 import java.math.BigInteger;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Rule Tester.
@@ -18,12 +20,56 @@ public class RuleTest {
 
     @Test
     public void testFlow() throws Exception {
-        BigInteger bigInteger = new BigInteger("11", 2);
+        BigInteger bigInteger = new BigInteger("11111111", 2);
         List<Step> steps = Rule.flow(bigInteger);
 
         for (Step step : steps) {
-            System.out.println(step.getValue().toString(2));
+            if (step.isOdd()) {
+                System.out.println(step.getValue().toString(2));
+            }
         }
     }
 
-} 
+    @Test
+    public void testAnaglize() throws Exception {
+        BigInteger bigInteger = new BigInteger("11111", 2);
+        Rule.anaglize(bigInteger).forEach(System.out::println);
+
+
+    }
+
+    @Test
+    public void testWhat() throws Exception {
+        final BigInteger maxInt = new BigInteger("111111111111111111111111111111111111111", 2);
+        BigInteger bigInteger = BigInteger.ZERO;
+        Map<String, Integer> results = new HashMap<>();
+        Map<Integer, BigInteger> map = new HashMap<>();
+        while (bigInteger.compareTo(maxInt) < 0) {
+            bigInteger = bigInteger.add(BigInteger.ONE);
+
+            int max = 0;
+            for (Step step = new Step(bigInteger); !step.isOne(); step = new Step(step.getNext())) {
+                final String str = step.getValue().toString(2);
+                if (results.containsKey(str)) {
+                    int what = results.get(str);
+                    max = max > what ? max : what;
+                    break;
+                }
+                int count = Rule.getMax1(str);
+                max = max > count ? max : count;
+                results.put(str, max);
+            }
+            if (!map.containsKey(max)) {
+                map.put(max, bigInteger);
+                System.out.println(bigInteger.toString(2) + ":" + max);
+            }
+        }
+
+    }
+
+    @Test
+    public void testGetMax1() {
+        System.out.println(Rule.getMax1("10000000000000000"));
+    }
+
+}
